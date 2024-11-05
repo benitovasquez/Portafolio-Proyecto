70e6eb552cbbbe0f2301d6e9a0155e1a8d6fe93a
benitovasquez <ricardobenitovasquezroca@gmail.com>
2024-11-05 10:20:25 -0400

    3.4. Creando componentes para blog

---
+++ assets/js/loadBlogComponents.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", function() {
+    loadComponent('blog', 'components/Blog.html');
+});
+
+function loadComponent(id, url) {
+    fetch(url)
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById(id).innerHTML = data;
+            // Ejecutar scripts dentro del componente cargado
+            const scripts = document.getElementById(id).getElementsByTagName('script');
+            for (let script of scripts) {
+                eval(script.innerText);
+            }
+        })
+        .catch(error => console.error('Error loading component:', error));
+}
