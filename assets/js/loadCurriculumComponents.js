ab6f77c71342768c75b2d6ca80218d06a17d4e34
benitovasquez <ricardobenitovasquezroca@gmail.com>
2024-11-05 09:36:00 -0400

    3.3. Creando componentes para curriculum

---
+++ assets/js/loadCurriculumComponents.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function() {
+    loadComponent('sidebar', 'components/Sidebar.html');
+    loadComponent('menu-toggle', 'components/MenuToggle.html');
+    loadComponent('main-content', 'components/curriculum/MainContent.html');
+});
+
+function loadComponent(id, url) {
+    fetch(url)
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById(id).innerHTML = data;
+            // Ejecutar scripts dentro del componente cargado
+            const scripts = document.getElementById(id).getElementsByTagName('script');
+            for (let script of scripts) {
+                eval(script.innerText);
+            }
+        })
+        .catch(error => console.error('Error loading component:', error));
+}
